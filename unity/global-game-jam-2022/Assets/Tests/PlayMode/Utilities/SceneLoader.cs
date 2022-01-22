63c9e0ad2f3103705e89f549571175f3133d8698
Andrew Allbright <aallbrig@gmail.com>
2022-01-22 11:00:39 -0500

    Add in time limit to scene loading

--- unity/global-game-jam-2022/Assets/Tests/PlayMode/Utilities/SceneLoader.cs
+++ unity/global-game-jam-2022/Assets/Tests/PlayMode/Utilities/SceneLoader.cs
@@ -1,22 +1,21 @@
 using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Tests.PlayMode.Utilities
 {
     public static class SceneLoader
     {
-        public static IEnumerator LoadTargetScene(string targetSceneName)
+        private const float TimeLimit = 1000 * 60; // one minute max
+        private static bool WithinTimeLimit(float startTime) => Time.time - startTime < TimeLimit;
+        public static IEnumerator LoadTargetScene(string targetSceneName, bool reload = false)
         {
             var currentScene = SceneManager.GetActiveScene();
+            if (!reload && currentScene.name == targetSceneName)
+                yield break;
+            var startTime = Time.time;
             var sceneAsync = SceneManager.LoadSceneAsync(targetSceneName, LoadSceneMode.Single);
-
-            while (sceneAsync.isDone == false) yield return null;
-            if (currentScene.name == targetSceneName)
-            {
-                var sceneAsyncUnload = SceneManager.UnloadSceneAsync(currentScene);
-                while (sceneAsyncUnload.isDone == false) yield return null;
-            }
+            while (sceneAsync.isDone == false && WithinTimeLimit(startTime)) yield return null;
         }
-
     }
 }
