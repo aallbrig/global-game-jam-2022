bb1b0d03bb9bc97c304d8db203ba51d47a9d5b69
Andrew Allbright <aallbrig@gmail.com>
2022-01-22 01:47:39 -0500

    Stub out more prefabs, backed by tests

---
+++ unity/global-game-jam-2022/Assets/Tests/PlayMode/PrefabTests/ElitePrefab.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using NUnit.Framework;
+using Tests.PlayMode.Utilities;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests.PlayMode.PrefabTests
+{
+    public class ElitePrefab
+    {
+        private const string PrefabLocation = "Prefabs/Elite";
+
+        [UnityTest]
+        public IEnumerator Prefab_Spawnable()
+        {
+            var sut = Object.Instantiate(Resources.Load<GameObject>(PrefabLocation), TestLocation.Next());
+            
+            yield return null;
+
+            Assert.NotNull(sut);
+        }
+    }
+}
